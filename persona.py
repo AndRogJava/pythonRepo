c4e08475483dbb2a369471318c4d09eb77bd5978
fede8ps <fpasquali96@gmail.com>
2022-02-18 00:09:25 -0800

    Classe lavoratore

--- persona.py
+++ persona.py
@@ -40,3 +40,48 @@ class Persona(metaclass=ABCMeta):
             if other._nome==self._nome and other._cognome==self._cognome and other._data_nascita==self._data_nascita:
                 return True
             return False 
+
+
+class Lavoratore(Persona, metaclass=ABCMeta):
+    
+    def __init__(self,nome, cognome, data_nascita, idbadge, mansione):
+        super().__init__(nome, cognome, data_nascita)
+        if (len(idbadge) != 8) or (idbadge.isalnum() == False):
+            raise ValueError('Inserire un IBAN valido.')
+        self._idbadge = idbadge
+        self._mansione = mansione
+        logger.info("E' stato inserito un lavoratore.")  
+    
+    def __str__(self):
+        return super().__str__() + ' ' + self._idbadge + ' ' + self._mansione
+
+    def __eq__(self, other):
+        if isinstance(other, Lavoratore):
+            if super().__eq__(other) and other._idbadge == self._idbadge and other._mansione == self._mansione:
+                return True
+        return False
+    
+    def __hash__(self):
+        return hash((super().__hash__(), self._idbadge, self._mansione))
+
+    def get_idbadge(self):
+        return self._idbadge
+
+    def set_idbadge(self, x):
+        if (len(x)<0) or (len(x)>8) or (x.isalnum() == False):
+            raise('Inserisci un id valido')
+        else:
+            self._idbadge = x
+    
+    def get_masnione(self):
+        return self._mansione
+    
+    def set_mansione(self, x):
+        if(len(x)<0) or (x.isalpha() == False):
+            raise('Inserisci una mansione valida')
+        else:
+            self._mansione = x
+
+    @abstractmethod
+    def stipendio(self):
+        pass
